55ee9ef57f5af675310f5dbaca709660cac6d910
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-22 13:16:46 +0530

    †ermux

--- resources/string.sh
+++ resources/string.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade -y
-pkg upgrade -y
-pkg install python wget -y
-wget https://raw.githubusercontent.com/AstroUB/AstroUB/resources/AstroSetup.py
-pip install telethon
+
 
 echo "
     _        _             
