6fd71e270d5bcf9b7be487c941d42ad134e6e83c
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-25 18:23:21 +0530

    «Astro-InlineSetup»

--- astro/plugins/_inlineSetup.py
+++ astro/plugins/_inlineSetup.py
@@ -17,40 +17,10 @@ from astro.plugins import astrostats, masterinfo
 from astro.plugins import PYTHON, ASTRO, OWNER_ID
 from astro.config import Config
 
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+START_TIME = datetime.datetime.now()
 
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-              remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
+uptime = f"{str(datetime.datetime.now() - START_TIME).split('.')[0]}"
 
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-      
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
-
-
-start = datetime.now()
-myid = bot.uid
-    # By @Alone_loverBoy
-end = datetime.now()
-(end - start).microseconds / 1000
-uptime = get_readable_time((time.time() - StartTime))
 
 if Config.SUDO_USERS:
 
