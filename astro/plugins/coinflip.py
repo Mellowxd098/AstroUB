303ca35d08fd339e49def70fcd6f105baea4a35d
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-27 23:06:12 +0530

    «Astro-Plugins»

---
+++ astro/plugins/coinflip.py
@@ -0,0 +1,20 @@
+import random
+import asyncio
+from astro import CMD_HELP
+from astro.utils import admin_cmd
+
+STRING = ["HEADS", "TAILS"]
+
+@astro.on(admin_cmd(pattern="flipcoin ?(.*)"))
+async def coin(ast):
+      await ast.edit("Coin On Flip👀")
+      await asyncio.sleep(1)
+      await ast.edit("🪙")
+      await asyncio.sleep(1)
+      await ast.edit("Ohhhhh......\nAnswer is")
+      await ast.sleep(1)
+      await event.edit(random.choice(STRING))
+    
+CMD_HELP.update({"coin": ".flipcoin\nUse - Get Random Heads Or Tails"
+  
+})
