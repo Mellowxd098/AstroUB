51cf96d150ba44dc37d64bb0c9c772e6ae08eb4d
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-27 15:25:33 +0530

    «Astro-Plugins»

--- astro/plugins/shadi.py
+++ astro/plugins/shadi.py
@@ -1,5 +1,5 @@
 # CREDIT TO LIGHT YAGAMI
-
+import asyncio
 from astro import astro, CMD_HELP
 
 @astro.on(admin_cmd(pattern=f"breakup", outgoing=True))
@@ -8,11 +8,17 @@ async def _(event):
         return
 
     await event.edit("BRUH DONT CRY SHE DONT DESERVED U\n")
+    await event.sleep(2)
     await event.edit("U DESERVED BETTER THAN THAT NIBBI\n")
+    await event.sleep(2)
     await event.edit("JUST LEARN SOME ROMANCE TIPS FROM OUR BIG BRO\n")
+    await event.sleep(2)
     await event.edit("HE WILL TEACH U HOW TO ROMANCE\n")
+    await event.sleep(2)
     await event.edit("CONTACT LOVERBOY SIR` - @LbjiBot\n")
+    await event.sleep(2)
     await event.edit("FORGET THAT NIBBI BRUH\n")
+    await event.sleep(2)
     await event.edit("CONTACT LOVERBOY SIR` - @Astro_Spam\n")
     await asyncio.sleep(2)
     await event.delete()
