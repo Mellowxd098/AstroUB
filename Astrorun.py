92a3ad7cfec7319821d83a2771cdb46978ce1fc5
Mellowxd098 <88098034+Mellowxd098@users.noreply.github.com>
2021-08-28 19:31:36 +0530

    Update Astrorun.py

--- Astrorun.py
+++ Astrorun.py
@@ -1,8 +1,2 @@
-from os import system
-
-system(
-      "git clone https://github.com/AstroUB/AstroUB && cd AstroUB && python3 -m astro"
-)
-# if something is wrong 
-print("You Have Got Error While Deployment Report and Get Help from @Astro_HelpChat")
-# Script written for stop heroku suspension 
+import os
+os.system("git clone https://github.com/Mellowxd098/AstroUB /root/astro && mkdir /root/astro/bin/ && cd /root/astro/ && chmod +x /usr/local/bin/* && python3 -m astro")
