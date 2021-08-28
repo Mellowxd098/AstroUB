063636dcd0a2671c77ebf64276742792dad385bb
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-28 10:12:05 +0530

    «Astro-Plugins»

--- astro/plugins/convert2mp3.py
+++ astro/plugins/convert2mp3.py
@@ -6,7 +6,7 @@ import asyncio
 import os
 import time
 from datetime import datetime
-
+from astro import StartTime
 from astro.utils import admin_cmd, progress
 
 
@@ -24,7 +24,7 @@ async def _(event):
     await event.edit("trying to download media file, to my local")
     try:
         start = datetime.now()
-        c_time = time.time()
+        c_time = StartTime
         downloaded_file_name = await borg.download_media(
             reply_message,
             Config.TMP_DOWNLOAD_DIRECTORY,
